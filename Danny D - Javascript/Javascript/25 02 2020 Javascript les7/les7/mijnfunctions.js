15754e87948ebeab6614179b536f3de2805360ca
mauritsWouters <wouters.maurits@gmail.com>
2020-05-08 16:14:29 +0200

    Create mijnfunctions.js

---
+++ Danny D - Javascript/Javascript/25 02 2020 Javascript les7/les7/mijnfunctions.js
@@ -0,0 +1,13 @@
+function dollarKoers() {
+  return 1.36;
+}
+
+function euroKoers() {
+  return 0.74;
+}
+
+function dollar_naar_euro(inputdollers) {
+  return inputdollers * euroKoers();
+}
+
+//dollar_naar_euro(100);
